f7b1da82c485ad460b1ffa5b556d1335320541a2
PurusThuc <thuc@purusgames.com>
2024-09-02 11:16:37 +0700

    + lab 2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_project/lab_1_scaffolding.dart';
+import 'package:flutter_project/lab_1/lab_1_scaffolding.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
